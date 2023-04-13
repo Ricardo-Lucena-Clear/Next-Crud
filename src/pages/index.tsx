46104ee8ebaf942cf90cc1bfd4216709049378ef
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-13 10:33:39 -0300

    Layout and Title Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-    
+    <div className={`
+    flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to bg-purple-500
+    text-white
+    `}>
+    <Layout titulo="Cadastro simples">
+      <span>Conteudo</span>
+    </Layout>
     </div>
   )
 }
